2f0bd2f0a4341a1256fda127ac61ecc66da39cc1
Yunzhe <iLoveLive@honoka.moe>
2017-09-26 21:37:25 +0800

    Make framework and add bootstrap.

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 # -*- coding: utf-8 -*-
-import flask
+from flask import Flask, request, session, g, redirect, url_for, abort, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def demo():
+    return render_template('Demo.html')
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080)
